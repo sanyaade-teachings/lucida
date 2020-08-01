6c18dcab6963ef1f885a264a84cb326785927d5b
Sanyaade <sanyaade@ks-allinone.cluster.local>
2020-08-01 16:10:25 +0100

    Update to Ubuntu 18.04, with new openCV build and Dockerfile_2020 update June 2020

--- tools/install_opencv.sh
+++ tools/install_opencv.sh
@@ -4,7 +4,7 @@ if [ -z $THREADS ]; then
 fi
 
 installCheck () {
-#  g++ check_opencv.cpp -o check_opencv
+  g++ check_opencv.cpp -o check_opencv
   if [[ $? -ne 0 ]]; then
     return 1
   else
@@ -18,26 +18,41 @@ if installCheck "$0"; then
   exit 0
 fi
 
-if [ ! -d opencv-4.4.0 ]; then
-  git clone https://github.com/opencv/opencv.git opencv-4.4.0
-  if [ $? -ne 0 ]; then
+# Get dependencies installed
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-22-dev
+
+#clone the latest openCV and contribute packages from GitHub
+#NOTE: You can check a specific version using: git checkout xxxx
+
+if [ ! -d opencv ]; then
+  git clone https://github.com/opencv/opencv.git
+  git clone https://github.com/opencv/opencv_contrib.git
+  
+if [ $? -ne 0 ]; then
     echo "Could not clone OpenCV!!! Please try again later..."
     exit 1
   fi
 fi
-cd "opencv-4.4.0" \
-  && git checkout "4.4.0" \
+
+cd opencv \
   && mkdir -p build \
   && cd build \
-  && cmake ..  \
+  && cmake -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ../ \
   && make -j$THREADS \
   && make -j$THREADS install \
   && cd ../../
+echo " openCV is now installed ";
 
-if installCheck "$0"; then
-  echo "OpenCV installed";
-  exit 0;
-else
-  echo "Failed to install OpenCV";
-  exit 1;
-fi
+# NOTE check done below is currently failing, needs review
+#if installCheck "$0"; then
+#  echo "OpenCV installed";
+#  exit 0;
+#else
+#  echo "Failed to install OpenCV";
+#  exit 1;
+#fi
